5a714417e00fbe1fe4c81b425933c2f39ee6c876
Schnretzl <wrhendley@gmail.com>
2024-08-29 23:48:44 -0500

    Combine code into one function

--- problem1.py
+++ problem1.py
@@ -1,7 +1,10 @@
 # 1. Exceptional Weather Forecast
 
 # Task 1: Start
-def get_temperature():
+# Task 2: Temperature Conversion
+# Task 3: User Experience
+# Task 4: Finally
+def convert_temperature():
     while True:
         try:
             temperature = float(input("Enter temperature in Fahrenheit: "))
@@ -9,23 +12,17 @@ def get_temperature():
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
+        #end try
+    #end while
 #end function
 
 
-# Task 2: Temperature Conversion
-def convert_temperature(temperature):
-    celsius = (temperature - 32) * 5.0 / 9.0
-    return celsius
-#end function
 
 
-# Task 3: User Experience
 
 
-# Task 4: Finally
+
 
 #================================================================
 
-temp = get_temperature()
-celsius = convert_temperature(temp)
-print(f"Celsius: {celsius:.1f}")
+convert_temperature()
