23ee2e5d58d7bd1faf76193959e15a069a4492df
Schnretzl <wrhendley@gmail.com>
2024-08-29 23:26:39 -0500

    Add task 1

---
+++ problem1.py
@@ -0,0 +1,16 @@
+# 1. Exceptional Weather Forecast
+
+# Task 1: Start
+def get_temperature():
+    temperature = float(input("Enter temperature in Fahrenheit: "))
+    return temperature
+#end function
+
+
+# Task 2: Temperature Conversion
+
+
+# Task 3: User Experience
+
+
+# Task 4: Finally
