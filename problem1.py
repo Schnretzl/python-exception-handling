5bcec6683d8d214b21c39b6f5e6b88af19d77749
Schnretzl <wrhendley@gmail.com>
2024-08-29 23:44:26 -0500

    Add task 2.  Move exception handling to get_temperature function

--- problem1.py
+++ problem1.py
@@ -2,15 +2,30 @@
 
 # Task 1: Start
 def get_temperature():
-    temperature = float(input("Enter temperature in Fahrenheit: "))
-    return temperature
+    while True:
+        try:
+            temperature = float(input("Enter temperature in Fahrenheit: "))
+            return temperature
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
 #end function
 
 
 # Task 2: Temperature Conversion
+def convert_temperature(temperature):
+    celsius = (temperature - 32) * 5.0 / 9.0
+    return celsius
+#end function
 
 
 # Task 3: User Experience
 
 
 # Task 4: Finally
+
+#================================================================
+
+temp = get_temperature()
+celsius = convert_temperature(temp)
+print(f"Celsius: {celsius:.1f}")
